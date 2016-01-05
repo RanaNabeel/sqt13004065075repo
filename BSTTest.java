3cb2931861771b12994932d4017624ebbe0a88a2
Rana Nabeel <13004065075@umt.edu.pk>
2016-01-05 21:27:23 +0500

    Unit test file BSTTest Code file

---
+++ BSTTest.java
@@ -0,0 +1,19 @@
+package binarysearchtree;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.NullPointerException;
+
+public class BinarySearchTreeTest {
+	BST bst =null;
+	@Before
+	public void initilize(){
+	bst=new BST();
+		
+	}
+
+}
